6d6921de181e6121329492ac6c6749f016bfb410
Praxitelis <praxitelis.nikolaou@gmail.com>
2018-08-08 19:32:53 +0300

    calculating total incomes, expenses and percentages

--- app.js
+++ app.js
@@ -15,6 +15,16 @@ var budgetController = (function() {
 	};
 
 
+	var calculateTotal = function(type){
+
+		var sum = 0;
+		data.allItems[type].forEach(function(cur) {
+			sum += cur.value;
+		});
+
+		data.totals[type] = sum;
+
+	};
 	
 
 	var data = {
@@ -25,7 +35,9 @@ var budgetController = (function() {
 		totals: {
 		exp: 0,
 		inc: 0
-		}
+		},
+		budget: 0,
+		percentage: -1 // -1 usually means non existent (at the beginning)
 	
 	};
 
@@ -56,6 +68,33 @@ var budgetController = (function() {
 			data.allItems[type].push(newItem);
 			// Return the new element
 			return newItem;
+		},
+
+		calculateBudget: function() {
+
+			// Calculate total income and expenses
+			calculateTotal('exp');
+			calculateTotal('inc');
+			// Calculate the budget: income - expenses
+			data.budget = data.totals.inc - data.totals.exp;
+
+
+			// Calculate the percentage of income that we spent
+			if (data.totals.inc > 0) {
+			data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+			} else {
+				data.percentage = -1;
+			}
+		},
+
+		getBudget: function() {
+			return {
+				budget: data.budget,
+				totalInc: data.totals.inc,
+				totalExp: data.totals.exp,
+				percentage: data.percentage
+			}
+
 		},
 
 		testing: function() {
@@ -171,13 +210,14 @@ var controller = (function(budgetCtrl, UICtrl) {
 	var updateBudget = function(){
 
 		// 1. Calculate the budget
-
+		budgetCtrl.calculateBudget();
 
 		// 2. Return Budget
 
+		var budget = budgetCtrl.getBudget();
 
 		// 3. Display the budget on the UI
-
+		console.log(budget);
 	};
 
 
@@ -201,7 +241,7 @@ var controller = (function(budgetCtrl, UICtrl) {
 		UICtrl.clearFields();
 
 		// 5. Calculate and update Budget
-		updateBudget();
+		updateBudget(); // called everytime we add an item
 
 		}
 
