8638f0b4de073957965244dbe57ca147fefcc692
Praxitelis <praxitelis.nikolaou@gmail.com>
2018-08-19 17:38:39 +0300

    calculate and update percentages

--- app.js
+++ app.js
@@ -6,7 +6,25 @@ var budgetController = (function() {
 		this.id = id;
 		this.description = description;
 		this.value = value;
+		this.percentage = -1; // for when its not defined
 	};
+
+
+	Expense.prototype.calcPercentage = function(totalIncome) {
+
+		if (totalIncome > 0) {
+			this.percentage = Math.round((this.value / totalIncome) * 100);
+		} else {
+			this.percentage = -1;
+		}
+
+	};
+
+	Expense.prototype.getPercentage = function() {
+
+		return this.percentage;
+
+	}
 
 	var Income = function(id, description, value) {
 		this.id = id;
@@ -110,6 +128,23 @@ var budgetController = (function() {
 				data.percentage = -1;
 			}
 		},
+
+		calculatePercentages: function() {
+
+			data.allItems.exp.forEach(function(cur) {
+				cur.calcPercentage(data.totals.inc);
+			});
+
+		},
+
+		getPercentages: function() {
+
+			var allPerc = data.allItems.exp.map(function(cur) {
+				return cur.getPercentage();
+			});
+			return allPerc;
+		},
+
 
 		getBudget: function() {
 			return {
@@ -280,6 +315,18 @@ var controller = (function(budgetCtrl, UICtrl) {
 	};
 
 
+	var updatePercentages = function(){
+
+		// 1. Calculate percentages
+		budgetCtrl.calculatePercentages();
+
+		// 2. Read percentages from the budget controller
+		var percentages = budgetCtrl.getPercentages();
+		// 3. Update the UI with the new percentages
+		console.log(percentages);
+	};
+
+
 
 	var ctrlAddItem = function() {
 		var input, newItem;
@@ -301,6 +348,9 @@ var controller = (function(budgetCtrl, UICtrl) {
 
 		// 5. Calculate and update Budget
 		updateBudget(); // called everytime we add an item
+
+		// 6. Calculate and update percentages
+		updatePercentages();
 
 		}
 
@@ -326,6 +376,8 @@ var controller = (function(budgetCtrl, UICtrl) {
 			UICtrl.deleteListItem(itemID);
 			// 3. Update and show the new budget
 			updateBudget();
+			// 4. Calculate and update percentages
+		updatePercentages();
 		}
 
 
