f67ec48ceb77c2226872d455442d95ea81137941
Praxitelis <praxitelis.nikolaou@gmail.com>
2018-08-21 02:04:10 +0300

    changing input fields' color when expence or income

--- app.js
+++ app.js
@@ -216,6 +216,13 @@ var UIController = (function() {
 		};
 
 
+		var nodeListForEach = function(list, callback) {
+				for (var i = 0; i < list.length; i++) {
+					callback(list[i], i);
+				}
+			};
+
+
 	return {
 		getInput: function() {
 
@@ -306,11 +313,7 @@ var UIController = (function() {
 
 			var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
 
-			var nodeListForEach = function(list, callback) {
-				for (var i = 0; i < list.length; i++) {
-					callback(list[i], i);
-				}
-			};
+			
 
 			nodeListForEach(fields, function(current, index){
 
@@ -337,6 +340,24 @@ var UIController = (function() {
 
 			year = now.getFullYear();
 			document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+
+		},
+
+
+		//changing css class for exp or inc
+		changedType: function() {
+
+			var fields = document.querySelectorAll(
+					DOMstrings.inputType + ',' +
+					DOMstrings.inputDescription + ',' +
+					DOMstrings.inputValue);
+				
+				nodeListForEach(fields, function(cur) {
+					cur.classList.toggle('red-focus');
+
+				});
+
+				document.querySelector(DOMstrings.inputBtn).classList.toggle('red');
 
 		},
 
@@ -371,6 +392,7 @@ var controller = (function(budgetCtrl, UICtrl) {
 		});
 
 		document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+		document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
 	};
 
 
